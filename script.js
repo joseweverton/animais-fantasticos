506d929cf296622948241e3811193ab4cdf2a488
jwcosta-br <jwcosta.br@gmail.com>
2023-07-27 08:00:13 -0300

    JS - Animação Scroll ao rolar a página

--- script.js
+++ script.js
@@ -66,6 +66,27 @@ const initScrollSuave = () => {
   });
 };
 
+const initAnimacaoScroll = () => {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = innerHeight * 0.6;
+    const animaScroll = () => {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    };
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+};
+
 initTabNav();
 initAccordion();
 initScrollSuave();
+initAnimacaoScroll()
+
+
