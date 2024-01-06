8e5a1900f7d04f07a6c5cebefbf7f9618389eb53
jwcosta-br <jwcosta.br@gmail.com>
2024-01-06 13:51:29 -0300

    Animação de numeros e Date Object horario de funcionamento

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -7,8 +7,11 @@ const initAnimacaoScroll = () => {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
         const isSectionVisible = sectionTop - windowMetade < 0;
-        if (isSectionVisible) section.classList.add("ativo");
-        else section.classList.remove("ativo");
+        if (isSectionVisible)
+          section.classList.add("ativo");
+        else if (section.classList.contains("ativo")) {
+          section.classList.remove("ativo")
+        };
       });
     };
     animaScroll();
--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-
+import initAnimaNumeros from "./modules/anima-numeros.js"
+import initFuncionamento from "./modules/funcionamento.js";
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
@@ -15,3 +16,6 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
+
